0853546791369d77fd33b8a7f1cf10c90ab5ee30
Erkan Hatipoglu <e109261@gmail.com>
2023-12-01 23:08:40 +0300

    Make two calculations

--- day_10_calculator_app/project_files/calculator_app.py
+++ day_10_calculator_app/project_files/calculator_app.py
@@ -1,24 +1,18 @@
-
-
 # Summation
 def add(n1, n2):
     return n1 + n2
-
 
 # Subtraction
 def subtract(n1, n2):
     return n1 - n2
 
-
 # Multiplication
 def multiply(n1, n2):
     return n1 * n2
 
-
 # Division
 def divide(n1, n2):
     return n1 / n2
-
 
 # Define operations
 operations = {
@@ -40,8 +34,23 @@ num2 = int(input("Enter second number: "))
 # define calculation function
 calculation_function = operations[operation_symbol]
 
-# get operation
+# make calculation
 answer = calculation_function(num1, num2)
 
 # print answer
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+# get operator
+operation_symbol = input("Pick another operation: ")
+
+# get input 3
+num3 = int(input("What is the next number?: "))
+
+# define calculation function
+calculation_function = operations[operation_symbol]
+
+# make calculation
+second_answer = calculation_function(answer, num3)
+
+# print answer
+print(f"{answer} {operation_symbol} {num3} = {second_answer}")
