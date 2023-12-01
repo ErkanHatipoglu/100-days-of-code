f9ef08d787fbad1b66ad77565d64ba091b2d709a
Erkan Hatipoglu <e109261@gmail.com>
2023-12-01 19:43:27 +0300

    Define basic functions

---
+++ day_10_calculator_app/project_files/calculator_app.py
@@ -0,0 +1,17 @@
+
+
+# Summation
+def: add(n1, n2):
+  return n1+n2
+  
+# Subtraction
+def: subtract(n1, n2):
+  return n1-n2
+  
+# Multiplication
+def: multiply(n1, n2):
+  return n1*n2
+  
+# Division
+def: divide(n1, n2):
+  return n1/n2
