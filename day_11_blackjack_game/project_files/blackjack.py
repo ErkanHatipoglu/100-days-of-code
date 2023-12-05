4c1868d030309e99aca25755510b3122b7edcce0
Erkan Hatipoglu <e109261@gmail.com>
2023-12-06 00:31:27 +0300

    Display cards

--- day_11_blackjack_game/project_files/blackjack.py
+++ day_11_blackjack_game/project_files/blackjack.py
@@ -7,52 +7,57 @@ clear = lambda: os.system('cls') # If you are not using replit use this.
 # Card list
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# Deck for player. Initially empty.
-player_deck = []
-
-# Deck for dealer. Initially empty.
-dealer_deck = []
-
 # Function to deal a card from the deck 
 def deal_card():
-  return random.choice(cards)
+	return random.choice(cards)
 
 # Function to play the game of Blackjack
 def play_game():
-  # Clear screen
-  clear()
+	# Reset decks and scores.
+	player_deck = []
+	dealer_deck = []
+	player_score = 0
+	dealer_score = 0
 
-  # Display logo
-  print(logo)
+	# Clear screen
+	clear()
 
-  # Deal card for player
-  for num in range(2):
-  	player_deck.append(deal_card())
+	# Display logo
+	print(logo)
 
-  # Deal card for dealer
-  for num in range(2):
-  	dealer_deck.append(deal_card())
+	# Deal card for player
+	for num in range(2):
+		card = int(deal_card())
+		player_score += card
+		player_deck.append(card)
 
-  # Test
-  print(f"Player Deck: {player_deck}")
-  print(f"Dealer Deck: {dealer_deck}")
-    
-  # Prompt the user to decide whether they want to play
-  user_request = input("Do you want to play blackjack? (yes/no) ")
-  
-  # If the user wants to play, recursively call the play_game function
-  if user_request == "yes":
-    play_game()
-  else:
-    # If the user chooses not to play, display a farewell message
-    print("Bye :(")
+
+	# Deal card for dealer
+	for num in range(2):
+		card = int(deal_card())
+		dealer_score += card
+		dealer_deck.append(card)
+
+	# Display Cards
+	print(f"Your cards: {player_deck}, current score: {player_score}")
+	print(f"Computer's first card: [{dealer_deck[0]}]")
+	
+	# Prompt the user to decide whether they want to play
+	user_request = input("Do you want to play blackjack? (yes/no) ")
+
+	# If the user wants to play, recursively call the play_game function
+	if user_request == "yes":	  	
+	  	play_game()
+	else:
+	    # If the user chooses not to play, display a farewell message
+	    print("Bye :(")
 
 # Prompt the user to decide whether they want to play before calling the play_game function
 user_request = input("Do you want to play blackjack? (yes/no) ")
 
 # If the user wants to play, initiate the game by calling the play_game function
 if user_request == "yes":
-  play_game()
+	play_game()
 else:
-  # If the user chooses not to play, display a farewell message
-  print("Bye :(")
+	# If the user chooses not to play, display a farewell message
+	print("Bye :(")
