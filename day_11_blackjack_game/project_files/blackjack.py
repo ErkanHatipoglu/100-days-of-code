322e4f72dcb9575a8e7e2fdc9deb81ba25930149
Erkan Hatipoglu <e109261@gmail.com>
2023-12-06 00:02:17 +0300

    Deal for player and dealer

--- day_11_blackjack_game/project_files/blackjack.py
+++ day_11_blackjack_game/project_files/blackjack.py
@@ -1,8 +1,17 @@
 import random
 from art import logo
+import os # If you are not using replit use this.
+clear = lambda: os.system('cls') # If you are not using replit use this.
+# from replit import clear # If you are using replit use this.
 
 # Card list
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# Deck for player. Initially empty.
+player_deck = []
+
+# Deck for dealer. Initially empty.
+dealer_deck = []
 
 # Function to deal a card from the deck 
 def deal_card():
@@ -10,9 +19,24 @@ def deal_card():
 
 # Function to play the game of Blackjack
 def play_game():
+  # Clear screen
+  clear()
+
   # Display logo
   print(logo)
-  
+
+  # Deal card for player
+  for num in range(2):
+  	player_deck.append(deal_card())
+
+  # Deal card for dealer
+  for num in range(2):
+  	dealer_deck.append(deal_card())
+
+  # Test
+  print(f"Player Deck: {player_deck}")
+  print(f"Dealer Deck: {dealer_deck}")
+    
   # Prompt the user to decide whether they want to play
   user_request = input("Do you want to play blackjack? (yes/no) ")
   
