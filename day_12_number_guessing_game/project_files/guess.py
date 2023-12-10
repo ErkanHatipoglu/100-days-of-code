9f3cab7912dbdaf9b6a25357b22f45bb9bfee1b1
Erkan Hatipoglu <e109261@gmail.com>
2023-12-10 12:23:35 +0300

    Generate a number, display welcome message and get difficulty level

---
+++ day_12_number_guessing_game/project_files/guess.py
@@ -0,0 +1,49 @@
+from art import logo
+from random import randint
+
+# Determine Range
+MIN_NUM = 1
+MAX_NUM = 100
+
+# Determine Difficulty
+EASY = 10
+HARD = 5
+DEFAULT = 0
+
+# User life
+user_life = 0
+
+def welcome():
+	"""
+    Displays a welcome message for the number guessing game and prompts the user to choose a difficulty level.
+
+    Returns:
+    - EASY (int): Represents the easy difficulty level.
+    - HARD (int): Represents the hard difficulty level.
+    - DEFAULT (int): Represents the default difficulty level if an invalid input is provided.
+
+    Usage Example:
+    difficulty_level = welcome()
+    """
+	print("Welcome to the Number Guessing Game!")
+	print(f"I am thinking of a number between {MIN_NUM} and {MAX_NUM}.")
+	user_input = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+	if user_input == "easy":
+		return EASY
+	elif user_input == "hard":
+		return HARD
+	else:
+		return DEFAULT
+
+# print logo
+print(logo)
+
+# Generate a random number
+number = randint(1,100)
+
+# Display welcome message and get level.
+user_life = welcome()
+
+# Test
+print (f"User Life: {user_life}")
+
