59ea70322d85617edd4988cc46f0844367aa7d5f
Erkan Hatipoglu <e109261@gmail.com>
2023-12-13 23:39:41 +0300

    Fix comments and docstrings

--- day_14_higher_lower_game/project_files/high_low.py
+++ day_14_higher_lower_game/project_files/high_low.py
@@ -20,28 +20,30 @@ def get_celebrity(game_data):
     return random.choice(game_data)
 
 def display_score(score):
-	"""
+    """
     Displays the current score.
 
     Parameters:
     score (int): The current score to display.
     """
-	print(f"You're right! Current score: {score}") 
+    print(f"You're right! Current score: {score}") 
 
 def display_celebrity(celebrity_dict, list_order):
-	if list_order == 0:
-		order = 'Compare A'
-	else:
-		order = 'Against B'
-	print(f"{order}: {celebrity_dict['name']}, a {celebrity_dict['description']}, from {celebrity_dict['country']}.")
+    """
+    Displays information about a celebrity.
 
+    Parameters:
+    celebrity_dict (dict): A dictionary containing data about a celebrity.
+    list_order (int): Indicates the position of the celebrity in the list (0 for 'Compare A', 1 for 'Against B').
+    """
+    order = 'Compare A' if list_order == 0 else 'Against B'
+    print(f"{order}: {celebrity_dict['name']}, a {celebrity_dict['description']}, from {celebrity_dict['country']}.")
 
 # Adding a randomly chosen celebrity to the celebrity list for display
 celebrity_list.append(get_celebrity(data))
 
 # Test display
 display_celebrity(celebrity_list[0], 0)
-
 display_celebrity(celebrity_list[0], 1)
 
 # Test score
