66cd624f9f1cd989c266d86609c16d27df7bf01b
Erkan Hatipoglu <e109261@gmail.com>
2023-12-16 23:13:32 +0300

    Create get_user_selection function

--- day_15_coffee_machine/project_files/coffee.py
+++ day_15_coffee_machine/project_files/coffee.py
@@ -1,4 +1,11 @@
-from art import logo
+"""This module simulates a virtual coffee machine. It provides users with various options like selecting a beverage
+from a predefined menu, turning the machine off, or getting a report on the current status of resources. The menu
+includes options like espresso, latte, and cappuccino, each with its own ingredients and cost."""
+
+from art import logo  # Importing the 'art' library to use the 'logo' graphic for the virtual coffee machine interface.
+
+# Defining the menu for the coffee machine. Each beverage type (espresso, latte, cappuccino) has its specific
+# ingredients and cost.
 MENU = {
     "espresso": {
         "ingredients": {
@@ -25,9 +32,40 @@ MENU = {
     }
 }
 
+# Defining the available resources in the coffee machine (water, milk, coffee) with their quantities.
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+
+def get_user_selection(menu):
+    """
+    Prompt the user to choose an action from the menu, which can be selecting a beverage, turning off the machine,
+    or getting a report on resources.
+
+    Args:
+    menu (dict): A dictionary containing the beverage options available in the menu.
+
+    Returns:
+    str: The user's selected action.
+    """
+    menu_options_str = ""
+    for key in menu:
+        menu_options_str += key + "/"
+    formatted_menu_items = menu_options_str.rstrip("/")  # Remove trailing slash for better formatting.
+    return input(f"What would you like? ({formatted_menu_items}): ")
+
+
+# Display the logo of the virtual coffee machine.
 print(logo)
+
+# Initialize variable to store the user's action.
+user_action = ""
+
+# Get the user's choice of action from the menu.
+user_action = get_user_selection(MENU)
+
+# Display the user's action for confirmation or debugging.
+print(f"User action is: {user_action}")
