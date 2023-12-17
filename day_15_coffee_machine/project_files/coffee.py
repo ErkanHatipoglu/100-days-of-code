57a2f239fca3edcc681fe64daa4fb9401877591e
Erkan Hatipoglu <e109261@gmail.com>
2023-12-17 20:35:55 +0300

    Create check_resources function

--- day_15_coffee_machine/project_files/coffee.py
+++ day_15_coffee_machine/project_files/coffee.py
@@ -33,11 +33,7 @@ MENU = {
 }
 
 # Defining the available resources in the coffee machine (water, milk, coffee) with their quantities.
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+resources = dict(water=300, milk=200, coffee=100)
 
 total_money = 0  # Tracks the total money accumulated by the coffee machine.
 
@@ -53,11 +49,8 @@ def get_user_selection(menu):
     Returns:
     str: The user's selected action.
     """
-    menu_options_str = ""
-    for key in menu:
-        menu_options_str += key + "/"
-    formatted_menu_items = menu_options_str.rstrip("/")  # Remove trailing slash for better formatting.
-    return input(f"What would you like? ({formatted_menu_items}): ").lower()
+    options = "/".join(menu)
+    return input(f"What would you like? ({options}): ").lower()
 
 
 def display_current_status(current_resources, money):
@@ -76,21 +69,63 @@ def display_current_status(current_resources, money):
     print(f"Money: ${money}")
 
 
+def check_resources(beverage, available_resources, menu):
+    """
+    Checks if the machine has enough resources to prepare the requested beverage.
+
+    Args:
+        beverage (str): Type of beverage to prepare.
+        available_resources (dict): Current quantities of resources in the machine.
+        menu (dict): Details of beverages and their required ingredients.
+
+    Returns:
+        bool: True if resources are sufficient, False otherwise.
+    """
+    water_amount = int(available_resources['water'])
+    milk_amount = int(available_resources['milk'])
+    coffee_amount = int(available_resources['coffee'])
+    missing_ingredients_list = []
+
+    if "water" in menu[beverage]['ingredients']:
+        if water_amount < menu[beverage]['ingredients']['water']:
+            missing_ingredients_list.append('water')
+
+    if "milk" in menu[beverage]['ingredients']:
+        if milk_amount < menu[beverage]['ingredients']['milk']:
+            missing_ingredients_list.append('milk')
+
+    if "coffee" in menu[beverage]['ingredients']:
+        if coffee_amount < menu[beverage]['ingredients']['coffee']:
+            missing_ingredients_list.append('coffee')
+
+    if len(missing_ingredients_list) > 0:
+        missing_ingredients = " and ".join(missing_ingredients_list)
+        print(f"Sorry there is not enough {missing_ingredients}!")
+        return False
+    else:
+        return True
+
+
 user_action = ""  # Stores the user's selected action.
 exit_program = False  # Flag to control the main program loop.
+print(logo)  # Display the logo of the virtual coffee machine.
 
 while not exit_program:
     # Main loop for operating the coffee machine. It continues until 'exit_program' is set to True.
-    print(logo)  # Display the logo of the virtual coffee machine.
 
     user_action = get_user_selection(MENU)  # Get the user's choice of action from the menu.
     print(f"User action is: {user_action}")  # Display the user's action for confirmation.
 
     if user_action == 'off':
+        print("Closing off. Please wait...")
         exit_program = True  # Exit the program if the user selects 'off'.
     elif user_action == 'report':
         display_current_status(resources, total_money)  # Display the current status.
     elif user_action in MENU.keys():
-        print('Check Resources')  # Placeholder for resource check.
+        sufficient_resources = check_resources(user_action, resources, MENU)
+        print(f"sufficient_resources: {sufficient_resources}")  # Test
+        print('Process Coins')  # Placeholder for coin process.
+        print('Check Transaction')  # Placeholder for transaction check.
+        print('Make Coffee')  # Placeholder for making coffee.
     else:
         print('Wrong Input')  # Inform the user of an incorrect input.
