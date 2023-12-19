ded4c75750b38c63b70d1c6c9523320192b08e90
Erkan Hatipoglu <e109261@gmail.com>
2023-12-20 00:22:31 +0300

    Insert comments

--- day_17_quiz_game/project_files/question_model.py
+++ day_17_quiz_game/project_files/question_model.py
@@ -1,4 +1,19 @@
 class Question:
+    """
+    A class to represent a quiz question.
+
+    Attributes:
+        text (str): The text of the quiz question.
+        answer (str): The answer to the quiz question (True/False).
+    """
+
     def __init__(self, text, answer):
+        """
+        The constructor for Question class.
+
+        Parameters:
+           text (str): The text of the quiz question.
+           answer (str): The answer to the quiz question (True/False).
+        """
         self.text = text
         self.answer = answer
