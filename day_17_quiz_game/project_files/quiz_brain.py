8bb1f42bfac4e6d03e5bd313ba71838c4fac87e2
Erkan Hatipoglu <e109261@gmail.com>
2023-12-20 13:30:02 +0300

    Create QuizBrain class

---
+++ day_17_quiz_game/project_files/quiz_brain.py
@@ -0,0 +1,30 @@
+class QuizBrain:
+    """
+    A class to manage the quiz game logic.
+
+    Attributes:
+        question_number (int): Tracks the current question number in the quiz.
+        question_list (list): A list of Question objects to be used in the quiz.
+    """
+
+    def __init__(self, question_list):
+        """
+        The constructor for the QuizBrain class.
+
+        Parameters:
+            question_list (list): A list of Question objects for the quiz.
+        """
+        self.question_number = 0  # Initialize the question number to zero
+        self.question_list = question_list  # Store the provided list of questions
+
+    def next_question(self):
+        """
+        Presents the next question to the user and increments the question number.
+        """
+        # Retrieve the next question from the list
+        new_question = self.question_list[self.question_number]
+        question_text = new_question.text
+        self.question_number += 1  # Increment the question number
+
+        # Display the question and prompt for user's answer
+        input(f"Q.{self.question_number}: {question_text} (True/False):")
