8303a003fccf22104cbb3de65df0ebc4e0a591b7
Erkan Hatipoglu <e109261@gmail.com>
2023-12-24 22:59:32 +0300

    Draw different shapes

--- day_18_turtle_scripts/project_files/main.py
+++ day_18_turtle_scripts/project_files/main.py
@@ -4,6 +4,8 @@ import random
 turtle = t.Turtle()
 t.colormode(255)
 
+colors = ["brown", "blue", "green", "red", "cyan", "orange", "deeppink"]
+
 def draw_square():
 	for i in range(4):
 		turtle.forward(100)
@@ -18,10 +20,32 @@ def draw_dashed_line():
 	    turtle.pd()
 
 
+def draw_shape(sides):
+	for y in range(sides):
+		turtle.forward(100)
+		turtle.right(360/sides)
+
+
+def draw_shapes():
+	for i in range(7):
+		turtle.pencolor(colors[i])
+		edge_num = i + 3
+		draw_shape(edge_num)
 
 draw_square()
 turtle.clear()
+turtle.setpos(0,0)
 draw_dashed_line()
+turtle.clear()
+turtle.penup()
+turtle.setpos(0,0)
+turtle.pendown()
+draw_shapes()
+
+
+
+
+    
 
 
 screen = t.Screen()
