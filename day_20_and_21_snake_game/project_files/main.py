e60888a3845737cf172b09672df7e61378a23c08
Erkan Hatipoglu <e109261@gmail.com>
2023-12-28 00:03:59 +0300

    Move snake forward

--- day_20_and_21_snake_game/project_files/main.py
+++ day_20_and_21_snake_game/project_files/main.py
@@ -1,6 +1,7 @@
+import time
 from turtle import Screen, Turtle
 
-SNAKE_BODY_LENGTH = 3
+SNAKE_BODY_PARTS = 3
 SNAKE_STARTING_X = 0
 SNAKE_STARTING_Y = 0
 SNAKE_PART_DIM = (20, 20)
@@ -9,8 +10,10 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Incredible Snake Game")
-
+screen.tracer(0)
 snake = []
+
+game_over = False
 
 
 def init_snake(length, x_position, y_position, part_dimensions):
@@ -25,8 +28,18 @@ def init_snake(length, x_position, y_position, part_dimensions):
     return new_snake
 
 
-snake = init_snake(SNAKE_BODY_LENGTH, SNAKE_STARTING_X, SNAKE_STARTING_Y, SNAKE_PART_DIM)
+snake = init_snake(SNAKE_BODY_PARTS, SNAKE_STARTING_X, SNAKE_STARTING_Y, SNAKE_PART_DIM)
 
 snake[0].color("red")
+
+while not game_over:
+    time.sleep(0.1)
+    screen.update()
+    total_parts = len(snake)
+    for part_number in range(total_parts - 1, 0, -1):
+        x_pos = snake[part_number - 1].xcor()
+        y_pos = snake[part_number - 1].ycor()
+        snake[part_number].goto(x_pos, y_pos)
+    snake[0].forward(20)
 
 screen.exitonclick()
