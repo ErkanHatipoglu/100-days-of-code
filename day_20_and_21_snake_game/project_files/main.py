67ad9a738b41eb5f47192e5cd19ba5e1506ddce1
Erkan Hatipoglu <e109261@gmail.com>
2023-12-28 22:42:03 +0300

    Import and use Snake class

--- day_20_and_21_snake_game/project_files/main.py
+++ day_20_and_21_snake_game/project_files/main.py
@@ -1,5 +1,7 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
+
+from snake import Snake
 
 SNAKE_BODY_PARTS = 3
 SNAKE_STARTING_X = 0
@@ -11,35 +13,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Incredible Snake Game")
 screen.tracer(0)
-snake = []
 
 game_over = False
 
-
-def init_snake(length, x_position, y_position, part_dimensions):
-    new_snake = []
-    for order in range(length):
-        s_part = Turtle("square")
-        s_part.penup()
-        s_part.color("white")
-        s_part.setx(x_position - (part_dimensions[0] * order))
-        s_part.sety(y_position)
-        new_snake.append(s_part)
-    return new_snake
-
-
-snake = init_snake(SNAKE_BODY_PARTS, SNAKE_STARTING_X, SNAKE_STARTING_Y, SNAKE_PART_DIM)
-
-snake[0].color("red")
+snake = Snake(length=SNAKE_BODY_PARTS, x_position=SNAKE_STARTING_X, y_position=SNAKE_STARTING_Y,
+              part_dimensions=SNAKE_PART_DIM)
+screen.update()
 
 while not game_over:
     time.sleep(0.1)
     screen.update()
-    total_parts = len(snake)
-    for part_number in range(total_parts - 1, 0, -1):
-        x_pos = snake[part_number - 1].xcor()
-        y_pos = snake[part_number - 1].ycor()
-        snake[part_number].goto(x_pos, y_pos)
-    snake[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
