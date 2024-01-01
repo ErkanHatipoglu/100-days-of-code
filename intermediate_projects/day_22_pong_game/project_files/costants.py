68a33fad232e0fa63c62e835b1213e646d3e1b1e
Erkan Hatipoglu <e109261@gmail.com>
2024-01-02 00:50:53 +0300

    Store game screen constants

---
+++ intermediate_projects/day_22_pong_game/project_files/costants.py
@@ -0,0 +1,9 @@
+# Game screen constants 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+SCREEN_COLOR = "black"
+SCREEN_TITLE = "The Great Pong Game"
+UP = 90
+PEN_COLOR = "white"
+PEN_SIZE = 5
+CENTER_LINE_HEIGHT = 12
