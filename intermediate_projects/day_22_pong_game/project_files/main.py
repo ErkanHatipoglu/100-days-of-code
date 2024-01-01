58315a5b15532e4ef7691bca7acda43d68b318b4
Erkan Hatipoglu <e109261@gmail.com>
2024-01-02 00:45:37 +0300

    Set up game screen

---
+++ intermediate_projects/day_22_pong_game/project_files/main.py
@@ -0,0 +1,44 @@
+from turtle import Screen, Turtle
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+SCREEN_COLOR = "black"
+SCREEN_TITLE = "The Great Pong Game"
+UP = 90
+PEN_COLOR = "white"
+PEN_SIZE = 5
+CENTER_LINE_HEIGHT = 12
+
+
+def initialize_turtle():
+    turtle = Turtle()
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.goto(0, - SCREEN_HEIGHT / 2)
+    turtle.setheading(UP)
+    turtle.pencolor(PEN_COLOR)
+    turtle.pensize(PEN_SIZE)
+    return turtle
+
+
+def draw_center_line():
+    """Draws a dashed line using turtle graphics."""
+    center_line_turtle = initialize_turtle()
+    for i in range(int(SCREEN_HEIGHT / (2 * CENTER_LINE_HEIGHT))):
+        center_line_turtle.forward(CENTER_LINE_HEIGHT)
+        center_line_turtle.pu()
+        center_line_turtle.forward(CENTER_LINE_HEIGHT)
+        center_line_turtle.pd()
+
+
+screen = Screen()
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.bgcolor(SCREEN_COLOR)
+screen.title(SCREEN_TITLE)  # Set window title
+screen.tracer(0)  # Turn off animation
+
+draw_center_line()
+
+screen.update()  # Update the screen with changes
+
+screen.exitonclick()
