60b12e053ab060d55f26d2a23a7f30ef1b9b0551
Erkan Hatipoglu <e109261@gmail.com>
2024-01-04 00:08:41 +0300

    Create up and down methods for paddle class

--- intermediate_projects/day_22_pong_game/project_files/paddle.py
+++ intermediate_projects/day_22_pong_game/project_files/paddle.py
@@ -10,3 +10,13 @@ class Paddle:
         self.paddle.goto(x_pos, y_pos)
         self.paddle.color(PADDLE_COLOR)
         self.paddle.shapesize(stretch_wid=WIDTH_STRETCH_CONSTANT, stretch_len=LENGTH_STRETCH_CONSTANT)
+
+    def up(self):
+        if self.paddle.ycor() < PADDLE_MAX_Y_POSITION:
+            new_y_pos = self.paddle.ycor() + PADDLE_SPEED
+            self.paddle.sety(new_y_pos)
+
+    def down(self):
+        if self.paddle.ycor() > - PADDLE_MAX_Y_POSITION:
+            new_y_pos = self.paddle.ycor() - PADDLE_SPEED
+            self.paddle.sety(new_y_pos)
