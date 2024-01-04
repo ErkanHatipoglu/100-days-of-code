40dc9bfa715ff2e84d53220edd0100fb8bc7fb58
Erkan Hatipoglu <e109261@gmail.com>
2024-01-04 19:18:23 +0300

    Inherit paddle from Turtle class

--- intermediate_projects/day_22_pong_game/project_files/paddle.py
+++ intermediate_projects/day_22_pong_game/project_files/paddle.py
@@ -3,20 +3,21 @@ from turtle import Turtle
 from constants import *
 
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, x_pos, y_pos):
-        self.paddle = Turtle("square")
-        self.paddle.penup()
-        self.paddle.goto(x_pos, y_pos)
-        self.paddle.color(PADDLE_COLOR)
-        self.paddle.shapesize(stretch_wid=WIDTH_STRETCH_CONSTANT, stretch_len=LENGTH_STRETCH_CONSTANT)
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.goto(x_pos, y_pos)
+        self.color(PADDLE_COLOR)
+        self.shapesize(stretch_wid=WIDTH_STRETCH_CONSTANT, stretch_len=LENGTH_STRETCH_CONSTANT)
 
     def up(self):
-        if self.paddle.ycor() < PADDLE_MAX_Y_POSITION:
-            new_y_pos = self.paddle.ycor() + PADDLE_SPEED
-            self.paddle.sety(new_y_pos)
+        if self.ycor() < PADDLE_MAX_Y_POSITION:
+            new_y_pos = self.ycor() + PADDLE_SPEED
+            self.sety(new_y_pos)
 
     def down(self):
-        if self.paddle.ycor() > - PADDLE_MAX_Y_POSITION:
-            new_y_pos = self.paddle.ycor() - PADDLE_SPEED
-            self.paddle.sety(new_y_pos)
+        if self.ycor() > - PADDLE_MAX_Y_POSITION:
+            new_y_pos = self.ycor() - PADDLE_SPEED
+            self.sety(new_y_pos)
