f6e423fede2e21b07d2322d2ff64aa2036cb1a2c
Erkan Hatipoglu <e109261@gmail.com>
2024-01-04 21:38:31 +0300

    Make the ball move

--- intermediate_projects/day_22_pong_game/project_files/ball.py
+++ intermediate_projects/day_22_pong_game/project_files/ball.py
@@ -1,3 +1,4 @@
+import random
 from turtle import Turtle
 
 from constants import *
@@ -11,3 +12,10 @@ class Ball(Turtle):
         self.goto(x_pos, y_pos)
         self.color(BALL_COLOR)
         self.fillcolor(BALL_COLOR)
+
+    def start(self):
+        angle = random.randint(-BALL_STARTING_ANGLE, BALL_STARTING_ANGLE)
+        self.setheading(angle)
+
+    def move(self):
+        self.forward(BALL_SPEED)
