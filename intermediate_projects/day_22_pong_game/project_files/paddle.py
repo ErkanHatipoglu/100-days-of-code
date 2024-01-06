59d796dfa9d6911863288f243b7714d6893db67a
Erkan Hatipoglu <e109261@gmail.com>
2024-01-06 22:23:54 +0300

    Insert comments

--- intermediate_projects/day_22_pong_game/project_files/paddle.py
+++ intermediate_projects/day_22_pong_game/project_files/paddle.py
@@ -1,10 +1,28 @@
+"""
+paddle.py for Pong Game
+This module defines the Paddle class used to create and manage the paddles in the Pong game.
+It includes methods for paddle creation, movement, and positioning."""
+
 from turtle import Turtle
 
 from constants import *
 
 
+# Paddle class represents a paddle in the game.
 class Paddle(Turtle):
+    """A class used to represent a paddle in the Pong game.
+
+    Attributes:
+        position (tuple): The starting position of the paddle.
+    """
+
+    # Initialize the paddle with a given position.
     def __init__(self, position):
+        """Initialize the paddle.
+
+        Parameters:
+            position (tuple): The starting position of the paddle.
+        """
         super().__init__()
         self.shape(PADDLE_SHAPE)
         self.penup()
@@ -12,12 +30,22 @@ class Paddle(Turtle):
         self.color(PADDLE_COLOR)
         self.shapesize(stretch_wid=WIDTH_STRETCH_CONSTANT, stretch_len=LENGTH_STRETCH_CONSTANT)
 
+    # Method to move the paddle up.
     def up(self):
+        """Move the paddle up by a certain amount.
+
+        This method changes the y-coordinate of the paddle to move it upwards.
+        """
         if self.ycor() < PADDLE_MAX_Y_POSITION:
             new_y_pos = self.ycor() + PADDLE_SPEED
             self.sety(new_y_pos)
 
+    # Method to move the paddle down.
     def down(self):
+        """Move the paddle down by a certain amount.
+
+        This method changes the y-coordinate of the paddle to move it downwards.
+        """
         if self.ycor() > - PADDLE_MAX_Y_POSITION:
             new_y_pos = self.ycor() - PADDLE_SPEED
             self.sety(new_y_pos)
