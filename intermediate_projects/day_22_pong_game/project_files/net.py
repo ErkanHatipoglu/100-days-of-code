ff49c0e8ecc06bfad97ec3d62301ba5abe70623b
Erkan Hatipoglu <e109261@gmail.com>
2024-01-06 22:48:13 +0300

    Insert comments

--- intermediate_projects/day_22_pong_game/project_files/net.py
+++ intermediate_projects/day_22_pong_game/project_files/net.py
@@ -1,10 +1,26 @@
+"""
+net.py for Pong Game
+This module defines the Net class used to create and manage the net in the middle of the Pong game field.
+It includes a method for drawing the net on the screen.
+"""
 from turtle import Turtle
 
 from constants import *
 
 
+# Net class represents the center net of the Pong game.
 class Net(Turtle):
+    """A class used to represent the net in the Pong game.
+
+    This class handles the creation and display of the net that divides the game field.
+    """
+
+    # Initialize the net with default settings.
     def __init__(self):
+        """Initialize the net.
+
+        Sets up the turtle graphics settings for drawing the net.
+        """
         super().__init__()
         self.hideturtle()
         self.penup()
@@ -13,8 +29,13 @@ class Net(Turtle):
         self.pencolor(PEN_COLOR)
         self.pensize(PEN_SIZE)
 
+    # Method to draw the net on the screen.
     def draw_net(self):
-        """Draws a dashed line using turtle graphics."""
+        """Draw the net on the game field.
+
+        This method creates the visual representation of the net.
+        Draws a dashed line using turtle graphics.
+        """
         for i in range(int(SCREEN_HEIGHT / (2 * CENTER_LINE_HEIGHT))):
             self.forward(CENTER_LINE_HEIGHT)
             self.pu()
