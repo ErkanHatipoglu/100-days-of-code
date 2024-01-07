a34909f58497b086cd5339008a7d69ae867a6984
Erkan Hatipoglu <e109261@gmail.com>
2024-01-07 22:10:55 +0300

    Create player class

--- intermediate_projects/day_23_turtle crossing_game/project_files/player.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/player.py
@@ -1,4 +1,22 @@
+from turtle import Turtle
+
 from constants import *
 
-class Player:
-    pass
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape(PLAYER_SHAPE)
+        self.penup()
+        self.setheading(UP)
+        self.goto(STARTING_POSITION)
+
+    def walk(self):
+        if self.ycor() < FINISH_LINE_Y + PLAYER_DIMENSIONS[1]:
+            new_y_pos = self.ycor() + MOVE_DISTANCE
+            self.sety(new_y_pos)
+        else:
+            self.restart()
+
+    def restart(self):
+        self.goto(STARTING_POSITION)
