c18ec49730a288ebc7ddf544ccfc13f8963ca198
Erkan Hatipoglu <e109261@gmail.com>
2024-01-07 22:11:35 +0300

    Add constants for Player class

--- intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
@@ -1,15 +1,24 @@
 # Game screen constants
+
 SCREEN_WIDTH = 600  # Width of the game screen in pixels
 SCREEN_HEIGHT = 600  # Height of the game screen in pixels
-SCREEN_COLOR = "white"  # Background color of the game screen
+
 SCREEN_TITLE = "Turtle Crossing Game"  # Title of the game window
+SCREEN_COLOR = "white"  # Background color of the game screen
+
 
 # Player Constants
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+PLAYER_SHAPE = "turtle"
+PLAYER_DIMENSIONS = (20, 20)  # Dimensions of player segments
+UP = 90
+
 # CarManager constants
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
