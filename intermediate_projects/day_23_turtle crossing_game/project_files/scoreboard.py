79af77f12b73394ac383632f5b0497260be4d8d4
Erkan Hatipoglu <e109261@gmail.com>
2024-01-08 00:28:30 +0300

    Create Scoreboard class

--- intermediate_projects/day_23_turtle crossing_game/project_files/scoreboard.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/scoreboard.py
@@ -1,4 +1,26 @@
+from turtle import Turtle
+
 from constants import *
 
-class Scoreboard:
-    pass
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.penup()
+        self.hideturtle()
+        self.pencolor(PEN_COLOR)
+        self.goto(SCOREBOARD_POSITION)
+        self.display_level()
+
+    def display_level(self):
+        self.write(arg=f"Level: {self.level}", move=False, font=FONT)
+
+    def increment_level(self):
+        self.clear()
+        self.level += 1
+        self.display_level()
+
+    def game_over(self):
+        self.goto(HOME)
+        self.write(arg=f"Game Over!", move=False, font=FONT)
