aa94d25bfc7c810f8d5d60066a640522bff0f887
Erkan Hatipoglu <e109261@gmail.com>
2024-01-07 23:54:22 +0300

    Add constants for CarManager class

--- intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
@@ -1,5 +1,4 @@
 # Game screen constants
-
 SCREEN_WIDTH = 600  # Width of the game screen in pixels
 SCREEN_HEIGHT = 600  # Height of the game screen in pixels
 
@@ -8,20 +7,29 @@ SCREEN_COLOR = "white"  # Background color of the game screen
 
 
 # Player Constants
-
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-PLAYER_SHAPE = "turtle"
 PLAYER_DIMENSIONS = (20, 20)  # Dimensions of player segments
+PLAYER_SHAPE = "turtle"
 UP = 90
 
 # CarManager constants
-
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+
+NUMBER_OF_CARS = 25
+CAR_SHAPE = "square"
+WIDTH_STRETCH_CONSTANT = 1
+LENGTH_STRETCH_CONSTANT = 2
+LEFT = 180
+CAR_MIN_STARTING_X_POS = 300
+CAR_MIN_STARTING_Y_POS = -270
+X_POSITION_SCALE_FACTOR = 1.5
+Y_POSITION_SCALE_FACTOR = 2
+CAR_DIMENSIONS = (40, 20)  # Dimensions of player segments
 
 # Scoreboard constants
 FONT = ("Courier", 24, "normal")
