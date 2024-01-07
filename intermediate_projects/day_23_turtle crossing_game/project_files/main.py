5ffa02ebed38b167c56a865adbeed590705ff5de
Erkan Hatipoglu <e109261@gmail.com>
2024-01-07 22:12:06 +0300

    Use Player class

--- intermediate_projects/day_23_turtle crossing_game/project_files/main.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 
+from player import Player
+
 from constants import *
 
 # Set up the main game screen using Turtle's Screen class.
@@ -9,6 +11,12 @@ screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 screen.bgcolor(SCREEN_COLOR)  # Setting the background color.
 screen.title(SCREEN_TITLE)  # Setting the window title.
 screen.tracer(0)  # Disabling automatic screen updates for manual control.
+
+player = Player()
+
+screen.listen()
+screen.onkeypress(fun=player.walk, key="Up")
+
 
 game_is_on = True
 while game_is_on:
