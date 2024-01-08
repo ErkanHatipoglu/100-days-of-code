39f10e781fd14e01baaa37d5366a4de18f40c986
Erkan Hatipoglu <e109261@gmail.com>
2024-01-08 23:11:37 +0300

    Reformat constants

--- intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
@@ -1,35 +1,20 @@
 # Game screen constants
 SCREEN_WIDTH = 600  # Width of the game screen in pixels
 SCREEN_HEIGHT = 600  # Height of the game screen in pixels
-
 SCREEN_TITLE = "Turtle Crossing Game"  # Title of the game window
 SCREEN_COLOR = "white"  # Background color of the game screen
-
 
 # Player Constants
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
-
-PLAYER_DIMENSIONS = (20, 20)  # Dimensions of player segments
+PLAYER_DIMENSIONS = (20, 20)
 PLAYER_SHAPE = "turtle"
 UP = 90
 
 # CarManager constants
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
-
 NUMBER_OF_CARS = 25
-CAR_SHAPE = "square"
-WIDTH_STRETCH_CONSTANT = 1
-LENGTH_STRETCH_CONSTANT = 2
-LEFT = 180
-CAR_MIN_STARTING_X_POS = 300
-CAR_MIN_STARTING_Y_POS = -270
-X_POSITION_SCALE_FACTOR = 1.5
-Y_POSITION_SCALE_FACTOR = 2
-CAR_DIMENSIONS = (40, 20)  # Dimensions of player segments
 COLLISION_DISTANCE = 30
 
 # Scoreboard constants
@@ -37,3 +22,16 @@ FONT = ("Courier", 24, "normal")
 PEN_COLOR = "black"
 SCOREBOARD_POSITION = (-280, 260)
 HOME = (-85, 0)
+
+# Car constants
+CAR_SHAPE = "square"
+LEFT = 180
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+WIDTH_STRETCH_CONSTANT = 1
+LENGTH_STRETCH_CONSTANT = 2
+MOVE_INCREMENT = 10
+CAR_MIN_STARTING_X_POS = 300
+CAR_MIN_STARTING_Y_POS = -270
+X_POSITION_SCALE_FACTOR = 1.5
+Y_POSITION_SCALE_FACTOR = 2
+CAR_DIMENSIONS = (40, 20)
