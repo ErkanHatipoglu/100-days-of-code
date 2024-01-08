b83592f0d5f76c42064c1c1ef69a8f0d7c0b13c0
Erkan Hatipoglu <e109261@gmail.com>
2024-01-09 00:34:27 +0300

    Insert comments and docstrings

--- intermediate_projects/day_23_turtle crossing_game/project_files/player.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/player.py
@@ -1,10 +1,21 @@
+# Importing the Turtle class from the turtle module
 from turtle import Turtle
 
+# Importing constants from the constants.py file
 from constants import *
 
 
 class Player(Turtle):
+    """
+    Player class, representing the player in the game, inherits from Turtle.
+    Handles the player movement and resetting the player's position.
+    """
+
     def __init__(self):
+        """
+        Constructor to initialize the player instance.
+        Sets the shape, pen up, heading, and starting position of the player.
+        """
         super().__init__()
         self.shape(PLAYER_SHAPE)
         self.penup()
@@ -12,8 +23,14 @@ class Player(Turtle):
         self.goto(STARTING_POSITION)
 
     def walk(self):
+        """
+        Moves the player upwards by a fixed distance.
+        """
         new_y_pos = self.ycor() + MOVE_DISTANCE
         self.sety(new_y_pos)
 
     def restart(self):
+        """
+        Resets the player's position to the starting position.
+        """
         self.goto(STARTING_POSITION)
