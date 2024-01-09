ee1fb27248bbd45a6b2613754151c0ada064825d
Erkan Hatipoglu <e109261@gmail.com>
2024-01-09 23:46:26 +0300

    Add comments

--- intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/constants.py
@@ -5,33 +5,33 @@ SCREEN_TITLE = "Turtle Crossing Game"  # Title of the game window
 SCREEN_COLOR = "white"  # Background color of the game screen
 
 # Player Constants
-STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
-FINISH_LINE_Y = 280
-PLAYER_DIMENSIONS = (20, 20)
-PLAYER_SHAPE = "turtle"
-UP = 90
+STARTING_POSITION = (0, -280)  # Initial position of the player
+MOVE_DISTANCE = 10  # Distance the player moves in one keystroke
+FINISH_LINE_Y = 280  # Y-coordinate of the finish line
+PLAYER_DIMENSIONS = (20, 20)  # Dimensions of the player
+PLAYER_SHAPE = "turtle"  # Shape of the player
+UP = 90  # Angle for upward movement
 
 # CarManager constants
-STARTING_MOVE_DISTANCE = 5
-NUMBER_OF_CARS = 25
-COLLISION_DISTANCE = 30
+STARTING_MOVE_DISTANCE = 5  # Initial move distance for cars
+NUMBER_OF_CARS = 25  # Total number of cars in the game
+COLLISION_DISTANCE = 30  # Distance for collision detection
 
 # Scoreboard constants
-FONT = ("Courier", 24, "normal")
-PEN_COLOR = "black"
-SCOREBOARD_POSITION = (-280, 260)
-HOME = (-85, 0)
+FONT = ("Courier", 24, "normal")  # Font style for the scoreboard text
+PEN_COLOR = "black"  # Pen color for the scoreboard text
+SCOREBOARD_POSITION = (-280, 260)  # Position of the scoreboard
+HOME = (-85, 0)  # Home position for certain text displays
 
 # Car constants
-CAR_SHAPE = "square"
-LEFT = 180
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-WIDTH_STRETCH_CONSTANT = 1
-LENGTH_STRETCH_CONSTANT = 2
-MOVE_INCREMENT = 10
-CAR_MIN_STARTING_X_POS = 300
-CAR_MIN_STARTING_Y_POS = -270
-X_POSITION_SCALE_FACTOR = 1.5
-Y_POSITION_SCALE_FACTOR = 2
-CAR_DIMENSIONS = (40, 20)
+CAR_SHAPE = "square"  # Shape of the cars
+LEFT = 180  # Angle for leftward movement
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]  # Possible colors of the cars
+WIDTH_STRETCH_CONSTANT = 1  # Width stretch factor for the car
+LENGTH_STRETCH_CONSTANT = 2  # Length stretch factor for the car
+MOVE_INCREMENT = 10  # Incremental increase in car movement speed
+CAR_MIN_STARTING_X_POS = 300  # Minimum X starting position for a car
+CAR_MIN_STARTING_Y_POS = -270  # Minimum Y starting position for a car
+X_POSITION_SCALE_FACTOR = 1.5  # Scale factor for random X position
+Y_POSITION_SCALE_FACTOR = 2  # Scale factor for random Y position
+CAR_DIMENSIONS = (40, 20)  # Dimensions of the car
