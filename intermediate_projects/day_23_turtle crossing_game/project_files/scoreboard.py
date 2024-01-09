b29056b7a4b96d6c417e36862db20917f70da57a
Erkan Hatipoglu <e109261@gmail.com>
2024-01-09 23:39:40 +0300

    Add comments and docstrings

--- intermediate_projects/day_23_turtle crossing_game/project_files/scoreboard.py
+++ intermediate_projects/day_23_turtle crossing_game/project_files/scoreboard.py
@@ -4,23 +4,41 @@ from constants import *
 
 
 class Scoreboard(Turtle):
+    """
+    Class representing the scoreboard in the game, inheriting from Turtle.
+    It handles the display and update of the player's level and the game over message.
+    """
+
     def __init__(self):
-        super().__init__()
-        self.level = 1
+        """
+        Initializes the scoreboard, setting up its appearance and initial level.
+        """
+        super().__init__()  # Initializing the Turtle superclass
+        self.level = 1  # Starting level
         self.penup()
-        self.hideturtle()
-        self.pencolor(PEN_COLOR)
-        self.goto(SCOREBOARD_POSITION)
-        self.display_level()
+        self.hideturtle()  # Hide the turtle icon
+        self.pencolor(PEN_COLOR)  # Set the pen color for the text
+        self.goto(SCOREBOARD_POSITION)  # Position of the scoreboard
+        self.display_level()  # Display the initial level
 
     def display_level(self):
+        """
+        Displays the current level on the screen.
+        """
         self.write(arg=f"Level: {self.level}", move=False, font=FONT)
 
     def increment_level(self):
-        self.clear()
-        self.level += 1
-        self.display_level()
+        """
+        Increments the level by one and updates the display.
+        This is called typically when the player passes a level.
+        """
+        self.clear()  # Clear the previous level text
+        self.level += 1  # Increment level
+        self.display_level()  # Display the new level
 
     def game_over(self):
-        self.goto(HOME)
-        self.write(arg=f"Game Over!", move=False, font=FONT)
+        """
+        Displays the 'Game Over' message at the end of the game.
+        """
+        self.goto(HOME)  # Position for the game over message
+        self.write(arg="Game Over!", move=False, font=FONT)
