e9ca3938fc698d121066e3a0beb2c28c3fd250f6
Erkan Hatipoglu <e109261@gmail.com>
2024-01-18 11:18:52 +0300

    Reproduce the code from day 25

--- intermediate_projects/day_25_us_states_game/project_files/main.py
+++ intermediate_projects/day_25_us_states_game/project_files/main.py
@@ -1,11 +1,18 @@
 import turtle
 
+
+def get_mouse_click_coordinate(x, y):
+    print(x, y)
+
+
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
+turtle.onscreenclick(get_mouse_click_coordinate)
 
+user_answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(user_answer)
 
-screen.exitonclick()
+turtle.mainloop()
