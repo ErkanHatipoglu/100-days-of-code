cbf569355e0ac0baefe8b5736ef002d740ec5d74
Erkan Hatipoglu <e109261@gmail.com>
2024-01-18 11:27:55 +0300

    Get states data

--- intermediate_projects/day_25_us_states_game/project_files/main.py
+++ intermediate_projects/day_25_us_states_game/project_files/main.py
@@ -1,5 +1,7 @@
 import turtle
 
+import pandas as pd
+
 
 def get_mouse_click_coordinate(x, y):
     print(x, y)
@@ -11,6 +13,9 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 turtle.onscreenclick(get_mouse_click_coordinate)
+
+states_data = pd.read_csv("50_states.csv")
+print(states_data)  # Test
 
 user_answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
 print(user_answer)
