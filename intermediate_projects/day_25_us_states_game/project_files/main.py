bfa24f45800f41e05b1ebc8ce2d53281251cb643
Erkan Hatipoglu <e109261@gmail.com>
2024-01-18 17:48:13 +0300

    Enhance US States Game with Answer Validation and Position Tracking
    - Implemented answer validation and case-insensitivity by converting all state names to lowercase and comparing user input in lowercase.
    - Added functionality to track and display the x and y coordinates of correctly guessed states using the `extract_result` function.
    - Improved game logic to remove guessed states from the remaining options, preventing repeated correct answers for the same state.
    - Refactored code by introducing utility functions like `get_key` and `check_user_answer` for cleaner and more modular code structure.
    - Incorporated a dictionary (`states_dict`) to manage state data, coordinates, and remaining states, streamlining the state tracking and updating process.
    - Updated the game loop to end the game when all states are guessed correctly by comparing `correct_answer_count` with `total_state_number`.

--- intermediate_projects/day_25_us_states_game/project_files/main.py
+++ intermediate_projects/day_25_us_states_game/project_files/main.py
@@ -7,6 +7,29 @@ def get_mouse_click_coordinate(x, y):
     print(x, y)
 
 
+def get_key(dictionary, dict_value):
+    for key, value in dictionary.items():
+        if dictionary[key] == dict_value:
+            return key
+
+
+def extract_result(dictionary, dict_value):
+    dictionary_key = get_key(dictionary["state"], dict_value)
+    x_pos = dictionary["x"].get(dictionary_key)
+    y_pos = dictionary["y"].get(dictionary_key)
+    print(f"x position: {x_pos}, y position:{y_pos}")
+    for _ in ["state", "x", "y"]:
+        dictionary[_].pop(dictionary_key)
+    return dictionary, x_pos, y_pos
+
+
+def check_user_answer(answer):
+    if answer is None:
+        return answer, True
+    else:
+        return answer.lower(), False
+
+
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
@@ -15,17 +38,28 @@ turtle.shape(image)
 turtle.onscreenclick(get_mouse_click_coordinate)
 
 states_data = pd.read_csv("50_states.csv")
-print(states_data)  # Test
-
-game_over = False
-user_answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+states_data["state"] = states_data["state"].str.lower()
 correct_answer_count = 0
 total_state_number = len(states_data)
+states_dict = states_data.to_dict()
+
+user_answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+user_answer, game_over = check_user_answer(user_answer)
+
+if user_answer in states_dict["state"].values():
+    correct_answer_count += 1
+    states_dict, x_position, y_position = extract_result(states_dict, user_answer)
+
 while not game_over:
     user_answer = screen.textinput(title=f"{correct_answer_count}/{total_state_number} States Correct",
                                    prompt="What's another state's name?")
-    print(user_answer)
-    if user_answer is None:
+    user_answer, game_over = check_user_answer(user_answer)
+
+    if user_answer in states_dict["state"].values():
+        correct_answer_count += 1
+        states_dict, x_position, y_position = extract_result(states_dict, user_answer)
+    if correct_answer_count == total_state_number:
         game_over = True
 
 turtle.mainloop()
