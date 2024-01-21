d35a7b2f6404049504c42ab55fee1678ea9ba6ad
Erkan Hatipoglu <e109261@gmail.com>
2024-01-21 23:06:00 +0300

    Introduce constants

--- intermediate_projects/day_25_us_states_game/project_files/main.py
+++ intermediate_projects/day_25_us_states_game/project_files/main.py
@@ -2,6 +2,17 @@ import turtle
 
 import pandas as pd
 
+SCREEN_TITLE = "US States Game"
+TEXT_INPUT_PROMPT = "What's another state's name?"
+IMAGE_FILE_PATH = "blank_states_img.gif"
+INPUT_FILE_PATH = "50_states.csv"
+OUTPUT_FILE_PATH = "states_to_learn.csv"
+TEXT_INPUT_INITIAL_TITLE = "Guess the State"
+EXIT_CONDITIONS = [None, "exit", "Exit", "EXIT", "quit", "Quit", "QUIT"]
+STATE_COLUMN = "state"
+X_COLUMN = "x"
+Y_COLUMN = "y"
+
 
 def get_mouse_click_coordinate(x, y):
     print(x, y)
@@ -14,16 +25,16 @@ def get_key(dictionary, dict_value):
 
 
 def extract_result(dictionary, dict_value):
-    dictionary_key = get_key(dictionary["state"], dict_value)
-    x_pos = dictionary["x"].get(dictionary_key)
-    y_pos = dictionary["y"].get(dictionary_key)
-    for _ in ["state", "x", "y"]:
+    dictionary_key = get_key(dictionary[STATE_COLUMN], dict_value)
+    x_pos = dictionary[X_COLUMN].get(dictionary_key)
+    y_pos = dictionary[Y_COLUMN].get(dictionary_key)
+    for _ in [STATE_COLUMN, X_COLUMN, Y_COLUMN]:
         dictionary[_].pop(dictionary_key)
     return dictionary, x_pos, y_pos
 
 
 def check_user_answer(answer):
-    if answer is None:
+    if answer in EXIT_CONDITIONS:
         return answer, True
     else:
         return answer.title(), False
@@ -38,38 +49,37 @@ def display_state(state_name, x_pos, y_pos):
 
 
 screen = turtle.Screen()
-screen.title("US States Game")
-image = "blank_states_img.gif"
+screen.title(SCREEN_TITLE)
+image = IMAGE_FILE_PATH
 screen.addshape(image)
 turtle.shape(image)
 turtle.onscreenclick(get_mouse_click_coordinate)
 
-states_data = pd.read_csv("50_states.csv")
-states_data["state"] = states_data["state"]
+states_data = pd.read_csv(INPUT_FILE_PATH)
 correct_answer_count = 0
 total_state_number = len(states_data)
 states_dict = states_data.to_dict()
 
-user_answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+user_answer = screen.textinput(title=TEXT_INPUT_INITIAL_TITLE, prompt=TEXT_INPUT_PROMPT)
 
 user_answer, game_over = check_user_answer(user_answer)
 
-if user_answer in states_dict["state"].values():
+if user_answer in states_dict[STATE_COLUMN].values():
     correct_answer_count += 1
     states_dict, x_position, y_position = extract_result(states_dict, user_answer)
     display_state(user_answer, x_position, y_position)
 
 while not game_over:
     user_answer = screen.textinput(title=f"{correct_answer_count}/{total_state_number} States Correct",
-                                   prompt="What's another state's name?")
+                                   prompt=TEXT_INPUT_PROMPT)
     user_answer, game_over = check_user_answer(user_answer)
 
-    if user_answer in states_dict["state"].values():
+    if user_answer in states_dict[STATE_COLUMN].values():
         correct_answer_count += 1
         states_dict, x_position, y_position = extract_result(states_dict, user_answer)
         display_state(user_answer, x_position, y_position)
     if correct_answer_count == total_state_number:
         game_over = True
 states_to_learn = pd.DataFrame(states_dict)
-states_to_learn.to_csv('states_to_learn.csv', index=False)
+states_to_learn.to_csv(OUTPUT_FILE_PATH, index=False)
 turtle.mainloop()
