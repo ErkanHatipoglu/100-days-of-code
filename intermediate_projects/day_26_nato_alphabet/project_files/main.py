43868bfa726638a7fd4ae1649924cddd5b187c84
Erkan Hatipoglu <e109261@gmail.com>
2024-01-29 00:19:24 +0300

    Finish TODO 2

--- intermediate_projects/day_26_nato_alphabet/project_files/main.py
+++ intermediate_projects/day_26_nato_alphabet/project_files/main.py
@@ -34,3 +34,7 @@ print(letter_to_phonetic_dict)
 { "A":"Alfa", "B":"Bravo" }
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+user_input = input("Enter a word: ").upper()
+letter_to_phonetic_list = [letter_to_phonetic_dict[letter] for letter in user_input]
+print(letter_to_phonetic_list)
