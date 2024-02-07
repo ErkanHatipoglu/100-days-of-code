765c4a261b096ad9ddddbef73b53190ea3729399
Erkan Hatipoglu <e109261@gmail.com>
2024-02-08 00:18:21 +0300

    Add tomato image to Pomodoro timer UI on canvas
    Introduced a canvas to the Pomodoro timer's UI and displayed a tomato image at the center. This enhances the visual appeal and aligns with the Pomodoro technique's theme.

--- intermediate_projects/day_28_pomodoro_gui_app/src/main.py
+++ intermediate_projects/day_28_pomodoro_gui_app/src/main.py
@@ -11,6 +11,8 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 WINDOW_TITLE = "Pomodoro"
+CANVAS_WIDTH = 200
+CANVAS_HEIGHT = 224
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -21,5 +23,10 @@ WINDOW_TITLE = "Pomodoro"
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title(WINDOW_TITLE)
+
+canvas = tk.Canvas(width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+tomato_image = tk.PhotoImage(file="assets/images/tomato.png")
+canvas.create_image(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, image=tomato_image)
+canvas.pack()
 
 window.mainloop()
